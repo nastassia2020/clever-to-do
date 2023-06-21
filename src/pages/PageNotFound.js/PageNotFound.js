37ecd74cd05ef4508c5a6d8ed182491120a2ed52
Nastassia Shylko <60188875+nastassia2020@users.noreply.github.com>
2023-06-21 17:02:33 +0300

    fixed prettier

--- src/pages/PageNotFound.js/PageNotFound.js
+++ src/pages/PageNotFound.js/PageNotFound.js
@@ -1,6 +1,5 @@
 const PageNotFound = () => {
-    return <h2 className="main"> Page not found</h2>;
-  };
-  
-  export default PageNotFound;
-  
+  return <h2 className="main"> Page not found</h2>;
+};
+
+export default PageNotFound;
